0eae03b9b8de5d5eb2c73e0fab3c65dba62cbcbe
kaptenPrice <49055785+kaptenPrice@users.noreply.github.com>
2020-02-10 15:42:29 +0100

    inloggningsvyn fixad

--- src/sample/InloggedCus.java
+++ src/sample/InloggedCus.java
@@ -1,28 +1,49 @@
 package sample;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
 public class InloggedCus {
-    @FXML
-    private Label buyTicketButton;
+
     @FXML
     private Label freeCoupons;
-    @FXML
-    private Label pesetasAmount;
+
     @FXML
     private Label MyBookingsLabel;
+
     @FXML
     private TextField searcTextField;
+
     @FXML
     private TextField showConsertTextField;
+
     @FXML
     private Button buyPesetas100;
+
     @FXML
     private Button buyPesetas200;
+
     @FXML
     private Button buyPesetas300;
+
+    @FXML
+    private Label pesetasAmount;
+
     @FXML
     private Button searchButton;
+
+    @FXML
+    private TextField choosenConsertIdTextField;
+
+    @FXML
+    private Button buyTicketButton;
+
+    @FXML
+    void buyTickets(ActionEvent event) {
+
+    }
 
 }
