a829d1db5a4328923d119584ca9a4bed86fc313d
mattiasfrigren <57172538+mattiasfrigren@users.noreply.github.com>
2020-02-10 15:43:13 +0100

    V1.1

---
+++ src/sample/InloggedCus.java
@@ -0,0 +1,30 @@
+package sample;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+public class InloggedCus {
+    @FXML
+    private Label buyTicketButton;
+    @FXML
+    private Label freeCoupons;
+    @FXML
+    private Label pesetasAmount;
+    @FXML
+    private Label MyBookingsLabel;
+    @FXML
+    private TextField searcTextField;
+    @FXML
+    private TextField showConsertTextField;
+    @FXML
+    private Button buyPesetas100;
+    @FXML
+    private Button buyPesetas200;
+    @FXML
+    private Button buyPesetas300;
+    @FXML
+    private Button searchButton;
+
+
+
+}
