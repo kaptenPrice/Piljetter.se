da08745d8daf3c3cde2b088b350e56938e78f605
mattiasfrigren <57172538+mattiasfrigren@users.noreply.github.com>
2020-02-11 13:34:46 +0100

    V1.1 fixed buy pesetas

--- src/sample/InloggedCus.java
+++ src/sample/InloggedCus.java
@@ -6,7 +6,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.sql.*;
+
 public class InloggedCus {
+    private Statement loggedin;
+    private LoginController loginController;
+
+    private int test;
 
     @FXML
     private Label freeCoupons;
@@ -46,4 +52,50 @@ public class InloggedCus {
 
     }
 
+    public void buy100Pesetas(ActionEvent actionEvent) throws SQLException {
+        loggedin = loginController.getLoginController().getLoginConnection().createStatement();
+        String login ="UPDATE cd.customer SET pesetas =(pesetas +100) WHERE customerid=" + "'" + loginController.getLoginController().getUserNameForInlog() + "'"
+                + " AND password= " + "'" + loginController.getLoginController().getPasswordForInlog() + "'";
+        loggedin.executeUpdate(String.format(login));
+        loggedin.close();
+        // loginController.getLoginController().getLoginConnection().close();
+        pesetasAmount.setText(String.valueOf(calculatePesetas()));
+
+    }
+
+    public void buy200Pesetas(ActionEvent actionEvent) throws SQLException {
+        loggedin = loginController.getLoginController().getLoginConnection().createStatement();
+        String login ="UPDATE cd.customer SET pesetas =(pesetas +200) WHERE customerid=" + "'" + loginController.getLoginController().getUserNameForInlog() + "'"
+                + " AND password= " + "'" + loginController.getLoginController().getPasswordForInlog() + "'";
+        loggedin.executeUpdate(String.format(login));
+        loggedin.close();
+        // loginController.getLoginController().getLoginConnection().close();
+        pesetasAmount.setText(String.valueOf(calculatePesetas()));
+
+    }
+
+    public void buy300Pesetas(ActionEvent actionEvent) throws SQLException {
+
+        loggedin = loginController.getLoginController().getLoginConnection().createStatement();
+        String login ="UPDATE cd.customer SET pesetas =(pesetas +300) WHERE customerid=" + "'" + loginController.getLoginController().getUserNameForInlog() + "'"
+                + " AND password= " + "'" + loginController.getLoginController().getPasswordForInlog() + "'";
+        loggedin.executeUpdate(String.format(login));
+        loggedin.close();
+       // loginController.getLoginController().getLoginConnection().close();
+        pesetasAmount.setText(String.valueOf(calculatePesetas()));
+    }
+    private int calculatePesetas() throws SQLException {
+
+        loggedin = loginController.getLoginController().getLoginConnection().createStatement();
+        String login ="SELECT pesetas FROM cd.customer WHERE customerid=" + "'" + loginController.getLoginController().getUserNameForInlog() + "'"
+                + " AND password= " + "'" + loginController.getLoginController().getPasswordForInlog() + "'";
+        ResultSet getPesetas = loggedin.executeQuery(login);
+        while (getPesetas.next()) {
+            test = getPesetas.getInt("pesetas");
+        }
+       return test;
+    }
+
+    public void search(ActionEvent actionEvent) {
+    }
 }
--- src/sample/LoginController.java
+++ src/sample/LoginController.java
@@ -15,9 +15,19 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class LoginController {
+    private static String passwordForInlog;
+    private static String userNameForInlog;
     private DBUtil data = new DBUtil();
-    private Connection loginConnection;
+    private static Connection loginConnection;
     private Statement loginstatment;
+    private static LoginController loginController;
+
+    public static LoginController getLoginController() {
+     if (loginController==null){
+         loginController = new LoginController();
+     }
+        return loginController;
+    }
 
     @FXML
     private TextField userNameBox,passWordBox;
@@ -28,13 +38,14 @@ public class LoginController {
     @FXML
     private void loginInButtonAction(ActionEvent event) throws IOException {
         System.out.println("HandlebuttonAction klickad ");
-        Parent homePageRoot = FXMLLoader.load(getClass().getResource("MysiteCustomer.fxml"));
+        Parent homePageRoot = FXMLLoader.load(getClass().getResource("NewUser.fxml"));
         Scene homePageScene = new Scene(homePageRoot);
         Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-       if (isValidCredentials()){
+       if (!userNameBox.getText().isEmpty() && isValidCredentials()){
            appStage.hide();
-        appStage.setScene(homePageScene);
-        appStage.show();}
+       // appStage.setScene(homePageScene);
+       // appStage.show();
+           }
        else {
            userNameBox.clear();
            passWordBox.clear();
@@ -64,7 +75,7 @@ public class LoginController {
   //              + " AND customerid= " + "'" + passWordBox.getText() + "'");
         try {
             loginConnection = DriverManager.getConnection(data.getDATABASECONNECTION(),data.getDATABASEINLOGG(),data.getDATABASEPASSWORD());
-            loginConnection.setAutoCommit(false);
+            //loginConnection.setAutoCommit(false);
             System.out.println("Opened db successfully");
             loginstatment = loginConnection.createStatement();
             String login ="SELECT * FROM cd.customer WHERE customerid=" + "'" + userNameBox.getText() + "'"
@@ -73,23 +84,44 @@ public class LoginController {
 
             while (loginresualt.next()) {
                 if (loginresualt.getString("customerid") != null && loginresualt.getString("password") != null) {
-                    String username = loginresualt.getString("customerid");
-                    System.out.println("customerid = " + username);
-                    String passWord = loginresualt.getString("password");
-                    System.out.println("password = " + passWord);
-
+                    userNameForInlog = loginresualt.getString("customerid");
+                    System.out.println("customerid = " + userNameForInlog);
+                    passwordForInlog = loginresualt.getString("password");
+                    System.out.println("password = " + passwordForInlog);
                     letIn = true;
+                    customerInlog();
                 }
             }
-            loginresualt.close();
-            loginstatment.close();
-            loginConnection.close();
+         //   loginresualt.close();
+       //     loginstatment.close();
+     //       loginConnection.close();
             System.out.println("closed conn.");
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
         }
         System.out.println("Operation done succesfully");
         return letIn;
+    }
+
+    private void customerInlog() throws IOException {
+        Parent inloggcus = FXMLLoader.load(getClass().getResource("InloggedCus.fxml"));
+        Stage inloggStage = new Stage();
+        inloggStage.setScene(new Scene(inloggcus));
+        inloggStage.show();
+    }
+
+
+
+    public String getPasswordForInlog() {
+        return passwordForInlog;
+    }
+
+    public String getUserNameForInlog() {
+        return userNameForInlog;
+    }
+
+    public Connection getLoginConnection() {
+        return loginConnection;
     }
 
 
