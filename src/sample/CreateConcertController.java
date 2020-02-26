173d9008aa3c39bcd39b946c78a45dbff97fad13
mattiasfrigren <57172538+mattiasfrigren@users.noreply.github.com>
2020-02-26 15:56:43 +0100

    V1.3 added cancelconsert, fixed create consert, fixed coupon functions and rewrote lost code

--- src/sample/CreateConcertController.java
+++ src/sample/CreateConcertController.java
@@ -6,16 +6,13 @@ import javafx.scene.control.TextField;
 import org.postgresql.util.PSQLException;
 
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ResourceBundle;
 
 import static java.lang.Integer.parseInt;
 
 public class CreateConcertController {
-
+    private int cost;
     private DBUtil dbUtil = new DBUtil();
     private Connection connection;
     private PreparedStatement preparedStatement;
@@ -25,12 +22,13 @@ public class CreateConcertController {
 
     @FXML
     private void createConcert() throws SQLException {
+        calculateConsertCost();
         connection = DriverManager.getConnection(dbUtil.getDATABASECONNECTION(), dbUtil.getDATABASEINLOGG(), dbUtil.getDATABASEPASSWORD());
         preparedStatement = connection.prepareStatement(
                 insertConcert + "VALUES(?,?,?,?,?)");
         preparedStatement.setString(1, artistNameValue.getText());
         preparedStatement.setString(2, sceneValue.getText());
-        preparedStatement.setInt(3, parseInt(costValue.getText()));
+        preparedStatement.setInt(3, cost);
         preparedStatement.setString(4, concertDateValue.getText());
         preparedStatement.setString(5, concertIdValue.getText());
 
@@ -44,6 +42,24 @@ public class CreateConcertController {
         System.out.println("create Statement");
         connection.close();
     }
+
+    private void calculateConsertCost() throws SQLException {
+        cost =0;
+        String konserCost = " SELECT cd.getkonsertcost((SELECT popularity FROM cd.artists WHERE name = '"+artistNameValue.getText()+
+                "'),(SELECT renomme FROM cd.places WHERE venue = '"+sceneValue.getText()+"'))";
+        try {
+            connection = DriverManager.getConnection(dbUtil.getDATABASECONNECTION(), dbUtil.getDATABASEINLOGG(), dbUtil.getDATABASEPASSWORD());
+            preparedStatement = connection.prepareStatement(konserCost);
+            ResultSet consertCost = preparedStatement.executeQuery();
+            while (consertCost.next()) {
+                cost =consertCost.getInt("getkonsertcost");
+            }
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
 
